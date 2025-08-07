f91b64035d011f054a2629e7fd082f0b5798832c
sulayman110 <piceofislam@gmail.com>
2025-08-07 11:45:49 +0600

    pactic-array-looping

---
+++ array-looping.js
@@ -0,0 +1,14 @@
+const fruits =[ 'apple','banana','anar','watermelon'];
+let i = 0;
+const k = [20,30,50,80,40,25]
+// for(let i=0 ;i<fruits.length ; i++){
+//     console.log(fruits[i])
+// }
+while(i < fruits.length){
+    console.log(fruits[i]);
+    i++;
+}
+while( i< k.length){
+    console.log(k[i]);
+    i++;
+}
