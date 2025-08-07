0cdb61039cc4a4ed6b1f08be7d030870b29ad4df
sulayman110 <piceofislam@gmail.com>
2025-08-07 12:57:21 +0600

    pactic-reverse

--- array-looping.js
+++ array-looping.js
@@ -4,10 +4,10 @@ const k = [20,30,50,80,40,25]
 // for(let i=0 ;i<fruits.length ; i++){
 //     console.log(fruits[i])
 // }
-while(i < fruits.length){
-    console.log(fruits[i]);
-    i++;
-}
+// while(i < fruits.length){
+//     console.log(fruits[i]);
+//     i++;
+// }
 while( i< k.length){
     console.log(k[i]);
     i++;
---
+++ reverse-looping.js
@@ -0,0 +1,14 @@
+const number = [1,2,3,4,5,6,7,8];
+const reverse =[];
+
+for( let i= number.length-1; i>=0; i--){
+    reverse.push(number[i]);
+}
+console.log(reverse)
+
+
+
+// for(let i=0;i < number.length; i++){
+//     console.log(number[i]);
+//     reverse.unshift(number[i])
+//     }console.log(reverse);
